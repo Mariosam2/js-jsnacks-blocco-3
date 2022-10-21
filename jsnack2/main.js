832263e65977ab16702a789dc52d4b0d64cc29c5
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-21 12:03:45 +0200

    added comments

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -2,9 +2,11 @@
 
 const stringsArray = ['ciao', 'Boh','PAROLA', 'pROVA', 'testo'];
 
-
+// la parola alla fine sarà in Camel Case
 const toCamelCase = stringsArray.map(string => {
+    // prendo il corpo della parola (senza lettera Iniziale) e lo metto in LowerCase
     let stringBody = string.slice(1 , string.length).toLocaleLowerCase();
+    //prendo la lettera iniziale e la metto in UpperCase
     let initialLetter = string[0].toLocaleUpperCase();
     string =  initialLetter + stringBody;
     return string;
