559d6a3b4e163898b9af1450c7c7c1fdf5c9f1e5
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-21 12:38:54 +0200

    edited snack track

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,5 +1,5 @@
 /* Crea un array di oggetti che rappresentano degli animali.
-Ogni animale ha un nome, una famiglia e una classe. */
+Ogni animale ha un nome, una famiglia e una classe. Crea un nuovo array con la lista dei mammiferi. */
 
 const animals = [
     {
