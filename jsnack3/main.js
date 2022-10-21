a704e18f1f1cc5828a119d502d7d590b98674500
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-21 12:36:52 +0200

    jsnack3

---
+++ jsnack3/main.js
@@ -0,0 +1,29 @@
+/* Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe. */
+
+const animals = [
+    {
+        nome: 'leone',
+        famiglia: 'felini',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'gallina',
+        famiglia: 'fasianidi',
+        classe: 'uccelli'
+    },
+    {
+        nome: 'balena',
+        famiglia: 'boh',
+        classe: 'mammiferi'
+    },
+]
+
+
+const mammals = animals.filter(animal => animal.classe === 'mammiferi');
+console.log(mammals);
